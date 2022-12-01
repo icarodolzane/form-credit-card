6d9e6f8a137857abc16f74499bc9929ae39d03b6
Icaro Dolzane <icarodolzane@live.com>
2022-12-01 12:56:51 -0400

    Criado o Header

---
+++ form-credit-card/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header.jsx'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header source='./images/shopLogo.svg'/>
+    </div>
+  );
+}
+
+export default App;
