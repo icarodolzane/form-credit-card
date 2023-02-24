035bd19b0379837093dc3c843b856fbdcc1e11b6
icarodolzane <icarodolzane@live.com>
2023-02-24 11:46:31 -0400

    Finalizado Função de deletar card

--- src/components/CardPreview.jsx
+++ src/components/CardPreview.jsx
@@ -5,18 +5,19 @@ import "../styles/components/CardPreview.css";
 
 function CardPreview() {
 
-  // const deleteCard = (number) => {
-  //   if(cards){
-  //     const cardsUpdated = cards.filter((card) => card.number !== number);
-  //     localStorage.removeItem('card');
-  //     localStorage.setItem(
-  //       'card',
-  //       JSON.stringify([...JSON.parse(localStorage.getItem('card')), 
-  //       cardsUpdated
-  //       ])
-  //     );  
-  //   }    
-  // };
+  const deleteCard = (number) => {
+    if(cards){
+      const cardsUpdated = cards.filter((card) => {
+        return card.number !== number
+      });
+      console.log(cardsUpdated);
+      localStorage.setItem(
+        'card',
+        JSON.stringify(cardsUpdated)
+      );
+      window.location.reload(false);  
+    }    
+  };
   const [cards, setCards] = useState([]);
 
   useEffect(() => {
@@ -38,7 +39,7 @@ function CardPreview() {
             />
             <Button
               variant="danger"
-              // onClick={() => deleteCard(card.number)}
+              onClick={() => deleteCard(card.number)}
             >
               Deletar cartão
             </Button>
